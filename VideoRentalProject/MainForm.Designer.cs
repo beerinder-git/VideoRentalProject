dee1978dcddd437913e84ff2fe8033e91c880fdc
katebi.milad <katebi.milad@yahoo.com>
2020-11-09 11:34:25 +1300

    Add project files.

---
+++ VideoRentalProject/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace VideoRentalProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.CustomerGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomerGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadBtn
+            // 
+            this.LoadBtn.Location = new System.Drawing.Point(12, 12);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(82, 54);
+            this.LoadBtn.TabIndex = 0;
+            this.LoadBtn.Text = "Load DB";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
+            // 
+            // CustomerGrid
+            // 
+            this.CustomerGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustomerGrid.Location = new System.Drawing.Point(12, 72);
+            this.CustomerGrid.Name = "CustomerGrid";
+            this.CustomerGrid.Size = new System.Drawing.Size(654, 293);
+            this.CustomerGrid.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CustomerGrid);
+            this.Controls.Add(this.LoadBtn);
+            this.Name = "MainForm";
+            this.Text = "Video Rental";
+            ((System.ComponentModel.ISupportInitialize)(this.CustomerGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadBtn;
+        private System.Windows.Forms.DataGridView CustomerGrid;
+    }
+}
+
---
+++ VideoRentalProject/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideoRentalProject
+{
+    public partial class MainForm : Form
+    {
+        string ConnectionString = @"Data Source=DHMATUT-09\SQLEXPRESS;Initial Catalog=VideoRental;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlConnection Connection;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Connection = new SqlConnection(ConnectionString);
+        }
+
+        private void LoadBtn_Click(object sender, EventArgs e)
+        {
+            Connection.Open();
+
+            DataTable CustomersTable = new DataTable();
+
+            CustomersTable.Clear();
+
+            CustomersTable.Columns.Add("CustID");
+            CustomersTable.Columns.Add("FirstName");
+            CustomersTable.Columns.Add("LastName");
+            CustomersTable.Columns.Add("Address");
+            CustomersTable.Columns.Add("Phone");
+
+            string query = "SELECT * FROM Customer";
+
+            SqlCommand command = new SqlCommand(query, Connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                CustomersTable.Rows.Add(
+                    reader["CustID"],
+                    reader["FirstName"],
+                    reader["LastName"],
+                    reader["Address"],
+                    reader["Phone"]
+                    );
+            }
+
+            CustomerGrid.DataSource = CustomersTable;
+
+            Connection.Close();
+        }
+    }
+}
