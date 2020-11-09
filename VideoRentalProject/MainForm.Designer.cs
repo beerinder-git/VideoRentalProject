35cf2d148ad5ddf4f5027bf8a8740da34b04e37a
katebi.milad <katebi.milad@yahoo.com>
2020-11-10 10:54:49 +1300

    Update and Delete are added

--- VideoRentalProject/MainForm.Designer.cs
+++ VideoRentalProject/MainForm.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.LoadBtn = new System.Windows.Forms.Button();
-            this.CustomerGrid = new System.Windows.Forms.DataGridView();
+            this.MainGrid = new System.Windows.Forms.DataGridView();
             this.LoadMovies = new System.Windows.Forms.Button();
             this.FNTBox = new System.Windows.Forms.TextBox();
             this.LNTBox = new System.Windows.Forms.TextBox();
@@ -40,7 +40,11 @@
             this.AdLabel = new System.Windows.Forms.Label();
             this.PhLabel = new System.Windows.Forms.Label();
             this.AddCustBtn = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.CustomerGrid)).BeginInit();
+            this.UpdCustBtn = new System.Windows.Forms.Button();
+            this.CustIDLabel = new System.Windows.Forms.Label();
+            this.CustIDTBox = new System.Windows.Forms.TextBox();
+            this.DltCustBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MainGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // LoadBtn
@@ -53,13 +57,14 @@
             this.LoadBtn.UseVisualStyleBackColor = true;
             this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
             // 
-            // CustomerGrid
+            // MainGrid
             // 
-            this.CustomerGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.CustomerGrid.Location = new System.Drawing.Point(12, 72);
-            this.CustomerGrid.Name = "CustomerGrid";
-            this.CustomerGrid.Size = new System.Drawing.Size(654, 293);
-            this.CustomerGrid.TabIndex = 1;
+            this.MainGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MainGrid.Location = new System.Drawing.Point(12, 72);
+            this.MainGrid.Name = "MainGrid";
+            this.MainGrid.Size = new System.Drawing.Size(654, 293);
+            this.MainGrid.TabIndex = 1;
+            this.MainGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.MainGrid_CellContentClick);
             // 
             // LoadMovies
             // 
@@ -73,28 +78,28 @@
             // 
             // FNTBox
             // 
-            this.FNTBox.Location = new System.Drawing.Point(110, 406);
+            this.FNTBox.Location = new System.Drawing.Point(378, 402);
             this.FNTBox.Name = "FNTBox";
             this.FNTBox.Size = new System.Drawing.Size(100, 20);
             this.FNTBox.TabIndex = 3;
             // 
             // LNTBox
             // 
-            this.LNTBox.Location = new System.Drawing.Point(216, 406);
+            this.LNTBox.Location = new System.Drawing.Point(272, 444);
             this.LNTBox.Name = "LNTBox";
             this.LNTBox.Size = new System.Drawing.Size(100, 20);
             this.LNTBox.TabIndex = 4;
             // 
             // AdTBox
             // 
-            this.AdTBox.Location = new System.Drawing.Point(322, 406);
+            this.AdTBox.Location = new System.Drawing.Point(378, 444);
             this.AdTBox.Name = "AdTBox";
             this.AdTBox.Size = new System.Drawing.Size(100, 20);
             this.AdTBox.TabIndex = 5;
             // 
             // PhTBox
             // 
-            this.PhTBox.Location = new System.Drawing.Point(428, 406);
+            this.PhTBox.Location = new System.Drawing.Point(484, 444);
             this.PhTBox.Name = "PhTBox";
             this.PhTBox.Size = new System.Drawing.Size(100, 20);
             this.PhTBox.TabIndex = 6;
@@ -102,7 +107,7 @@
             // FNLabel
             // 
             this.FNLabel.AutoSize = true;
-            this.FNLabel.Location = new System.Drawing.Point(110, 390);
+            this.FNLabel.Location = new System.Drawing.Point(378, 386);
             this.FNLabel.Name = "FNLabel";
             this.FNLabel.Size = new System.Drawing.Size(57, 13);
             this.FNLabel.TabIndex = 7;
@@ -111,7 +116,7 @@
             // LNLabel
             // 
             this.LNLabel.AutoSize = true;
-            this.LNLabel.Location = new System.Drawing.Point(213, 390);
+            this.LNLabel.Location = new System.Drawing.Point(269, 428);
             this.LNLabel.Name = "LNLabel";
             this.LNLabel.Size = new System.Drawing.Size(58, 13);
             this.LNLabel.TabIndex = 8;
@@ -120,7 +125,7 @@
             // AdLabel
             // 
             this.AdLabel.AutoSize = true;
-            this.AdLabel.Location = new System.Drawing.Point(319, 390);
+            this.AdLabel.Location = new System.Drawing.Point(375, 428);
             this.AdLabel.Name = "AdLabel";
             this.AdLabel.Size = new System.Drawing.Size(45, 13);
             this.AdLabel.TabIndex = 9;
@@ -129,7 +134,7 @@
             // PhLabel
             // 
             this.PhLabel.AutoSize = true;
-            this.PhLabel.Location = new System.Drawing.Point(425, 390);
+            this.PhLabel.Location = new System.Drawing.Point(481, 428);
             this.PhLabel.Name = "PhLabel";
             this.PhLabel.Size = new System.Drawing.Size(38, 13);
             this.PhLabel.TabIndex = 10;
@@ -139,17 +144,58 @@
             // 
             this.AddCustBtn.Location = new System.Drawing.Point(12, 384);
             this.AddCustBtn.Name = "AddCustBtn";
-            this.AddCustBtn.Size = new System.Drawing.Size(82, 54);
+            this.AddCustBtn.Size = new System.Drawing.Size(62, 54);
             this.AddCustBtn.TabIndex = 11;
             this.AddCustBtn.Text = "Add Customer";
             this.AddCustBtn.UseVisualStyleBackColor = true;
             this.AddCustBtn.Click += new System.EventHandler(this.AddCustBtn_Click);
             // 
+            // UpdCustBtn
+            // 
+            this.UpdCustBtn.Location = new System.Drawing.Point(80, 384);
+            this.UpdCustBtn.Name = "UpdCustBtn";
+            this.UpdCustBtn.Size = new System.Drawing.Size(62, 54);
+            this.UpdCustBtn.TabIndex = 12;
+            this.UpdCustBtn.Text = "Update Customer";
+            this.UpdCustBtn.UseVisualStyleBackColor = true;
+            this.UpdCustBtn.Click += new System.EventHandler(this.UpdCustBtn_Click);
+            // 
+            // CustIDLabel
+            // 
+            this.CustIDLabel.AutoSize = true;
+            this.CustIDLabel.Location = new System.Drawing.Point(272, 386);
+            this.CustIDLabel.Name = "CustIDLabel";
+            this.CustIDLabel.Size = new System.Drawing.Size(65, 13);
+            this.CustIDLabel.TabIndex = 14;
+            this.CustIDLabel.Text = "Customer ID";
+            // 
+            // CustIDTBox
+            // 
+            this.CustIDTBox.Enabled = false;
+            this.CustIDTBox.Location = new System.Drawing.Point(272, 402);
+            this.CustIDTBox.Name = "CustIDTBox";
+            this.CustIDTBox.Size = new System.Drawing.Size(100, 20);
+            this.CustIDTBox.TabIndex = 13;
+            // 
+            // DltCustBtn
+            // 
+            this.DltCustBtn.Location = new System.Drawing.Point(148, 384);
+            this.DltCustBtn.Name = "DltCustBtn";
+            this.DltCustBtn.Size = new System.Drawing.Size(62, 54);
+            this.DltCustBtn.TabIndex = 15;
+            this.DltCustBtn.Text = "Delete Customer";
+            this.DltCustBtn.UseVisualStyleBackColor = true;
+            this.DltCustBtn.Click += new System.EventHandler(this.DltCustBtn_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 476);
+            this.Controls.Add(this.DltCustBtn);
+            this.Controls.Add(this.CustIDLabel);
+            this.Controls.Add(this.CustIDTBox);
+            this.Controls.Add(this.UpdCustBtn);
             this.Controls.Add(this.AddCustBtn);
             this.Controls.Add(this.PhLabel);
             this.Controls.Add(this.AdLabel);
@@ -160,11 +206,11 @@
             this.Controls.Add(this.LNTBox);
             this.Controls.Add(this.FNTBox);
             this.Controls.Add(this.LoadMovies);
-            this.Controls.Add(this.CustomerGrid);
+            this.Controls.Add(this.MainGrid);
             this.Controls.Add(this.LoadBtn);
             this.Name = "MainForm";
             this.Text = "Video Rental";
-            ((System.ComponentModel.ISupportInitialize)(this.CustomerGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MainGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,7 +219,7 @@
         #endregion
 
         private System.Windows.Forms.Button LoadBtn;
-        private System.Windows.Forms.DataGridView CustomerGrid;
+        private System.Windows.Forms.DataGridView MainGrid;
         private System.Windows.Forms.Button LoadMovies;
         private System.Windows.Forms.TextBox FNTBox;
         private System.Windows.Forms.TextBox LNTBox;
@@ -184,6 +230,10 @@
         private System.Windows.Forms.Label AdLabel;
         private System.Windows.Forms.Label PhLabel;
         private System.Windows.Forms.Button AddCustBtn;
+        private System.Windows.Forms.Button UpdCustBtn;
+        private System.Windows.Forms.Label CustIDLabel;
+        private System.Windows.Forms.TextBox CustIDTBox;
+        private System.Windows.Forms.Button DltCustBtn;
     }
 }
 
--- VideoRentalProject/MainForm.cs
+++ VideoRentalProject/MainForm.cs
@@ -53,7 +53,7 @@ namespace VideoRentalProject
                     );
             }
 
-            CustomerGrid.DataSource = CustomersTable;
+            MainGrid.DataSource = CustomersTable;
 
             Connection.Close();
         }
@@ -94,7 +94,7 @@ namespace VideoRentalProject
                     );
             }
 
-            CustomerGrid.DataSource = CustomersTable;
+            MainGrid.DataSource = CustomersTable;
 
             Connection.Close();
         }
@@ -115,6 +115,54 @@ namespace VideoRentalProject
 
                 command.ExecuteNonQuery();
             }
+
+            Connection.Close();
+
+            LoadBtn_Click(null, null);
+        }
+
+        private void UpdCustBtn_Click(object sender, EventArgs e)
+        {
+            Connection.Open();
+
+            string query = "UPDATE Customer SET FirstName = @FirstName, LastName = @LastName, Phone = @Phone, Address = @Address WHERE CustID = " + CustIDTBox.Text + "; ";
+
+            using (SqlCommand command = new SqlCommand(query, Connection))
+            {
+                command.Parameters.Add("@FirstName", SqlDbType.NVarChar).Value = FNTBox.Text;
+                command.Parameters.Add("@LastName", SqlDbType.NVarChar).Value = LNTBox.Text;
+                command.Parameters.Add("@Phone", SqlDbType.NVarChar).Value = PhTBox.Text;
+                command.Parameters.Add("@Address", SqlDbType.NVarChar).Value = AdTBox.Text;
+
+                command.ExecuteNonQuery();
+            }
+
+            Connection.Close();
+
+            LoadBtn_Click(null, null);
+        }
+
+        private void MainGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            DataGridViewRow row = MainGrid.Rows[index];
+
+            CustIDTBox.Text = row.Cells[0].Value.ToString();
+            FNTBox.Text = row.Cells[1].Value.ToString();
+            LNTBox.Text = row.Cells[2].Value.ToString();
+            AdTBox.Text = row.Cells[3].Value.ToString();
+            PhTBox.Text = row.Cells[4].Value.ToString();
+        }
+
+        private void DltCustBtn_Click(object sender, EventArgs e)
+        {
+            Connection.Open();
+
+            string query = "DELETE Customer WHERE CustID = " + CustIDTBox.Text;
+
+            SqlCommand command = new SqlCommand(query, Connection);
+
+            command.ExecuteNonQuery();
 
             Connection.Close();
 
